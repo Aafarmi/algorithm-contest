ebd6b18c59a4c0060c26840a4213ae0c15f20289
Srend <820099292@qq.com>
2026-04-04 23:21:25 +0800

    ICPC

---
+++ Contest/ICPC/25NanChang/A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+//#define int long long
+#define ll long long
+#define db double
+#define fr first
+#define sc second
+#define endl '\n'
+#define all(arr) arr.begin(),arr.end()
+typedef pair<int,int> P;
+const int INF=0x3f3f3f3f;
+const ll mod=1e9+7;
+const ll _mod=998244353;
+const int N=2e5+7;
+void Srend(){
+    int a,b,c,d;cin>>a>>b>>c>>d;
+    int tot=a+b+c;
+    int ans=tot*d;
+    cout<<ans;
+} 
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    //cin>>t;
+    while(t--) Srend();
+}
